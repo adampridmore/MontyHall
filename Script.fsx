1ebf457d7e471ea37dbad0f81d22c7933f485313
Adam Pridmore <adam_pridmore@hotmail.com>
2018-01-22 21:55:35 +0000

    - Renamed script extension

---
+++ Script.fsx
@@ -0,0 +1,9 @@
+#load "MontyHall.fs"
+
+open MontyHall
+
+seq{0..10000}
+|> Seq.map (fun _ -> playGame() )
+//|> Seq.map (fun (game, win) -> game |> printGame; (game,win) )
+|> Seq.where snd
+|> Seq.length
